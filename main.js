a069e49eaab6dd313df11a8ad728bf9be0dcae43
Kaushal <Kausraj11@gmail.com>
2023-03-08 01:37:56 +0530

    Changes Made

--- main.js
+++ main.js
@@ -14,6 +14,14 @@ function scrollFunction() {
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    var time = 500; // Scroll duration in milliseconds
+    var step = -window.pageYOffset / (time / 15);
+    var timer = setInterval(function () {
+        window.scrollBy(0, step);
+        if (window.pageYOffset <= 0) {
+            clearInterval(timer);
+        }
+    }, 15);
 }
 
 
