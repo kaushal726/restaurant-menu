b5dc5cb1b479bbb830bd5374d5f19ff7a337e399
Kaushal <Kausraj11@gmail.com>
2023-03-08 01:06:50 +0530

    Changes Made

---
+++ main.js
@@ -0,0 +1,21 @@
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    var myBtn = document.getElementById("myBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myBtn.style.display = "block";
+    } else {
+        myBtn.style.display = "none";
+    }
+}
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
+
